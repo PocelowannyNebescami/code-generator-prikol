1c33d174e7591ca75b8b08392eb5051406dfddfd
Sergey <sergfox321@gmail.com>
2024-02-29 21:43:41 +0300

    Basic code generation was added

--- generator/methods.go
+++ generator/methods.go
@@ -1,7 +1,39 @@
 package generator
 
-import "strings"
+import (
+	"math/rand"
+	"strings"
+)
 
-func (generator Generator) Generate() string {
-	return strings.Repeat("0", generator.Length)
+func (generator Generator) Generate() (result string) {
+	var alphabet string
+	if generator.UseLowerCaseLetters {
+		alphabet += lowerCaseLetters
+	}
+	if generator.UseUpperCaseLetters {
+		alphabet += upperCaseLetters
+	}
+	if generator.UseNumbers {
+		alphabet += numbers
+	}
+	if generator.UseSymbols {
+		alphabet += symbols
+	}
+	if generator.ExcludeSimilarCharacters {
+		generator.Exclude += similarCharacters
+	}
+	for _, letter := range generator.Exclude {
+		alphabet = strings.ReplaceAll(alphabet, string(letter), "")
+	}
+
+	if len(alphabet) == 0 {
+		return result
+	}
+
+	randGenrator := rand.New(rand.NewSource(1337))
+	for index := uint(0); index < generator.Length; index++ {
+		randIndex := randGenrator.Intn(len(alphabet))
+		result += string(alphabet[randIndex])
+	}
+	return result
 }
